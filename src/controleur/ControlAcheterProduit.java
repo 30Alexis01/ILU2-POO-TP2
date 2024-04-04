faec0c7465188e51bc8cc8d94e820f6d05edf93f
30Alexis01 <alexisdieu3001@gmail.com>
2024-04-04 17:47:22 +0200

    TP2-fin

--- src/controleur/ControlAcheterProduit.java
+++ src/controleur/ControlAcheterProduit.java
@@ -1,19 +1,54 @@
 package controleur;
 
+import villagegaulois.Etal;
 import villagegaulois.Village;
+import personnages.Gaulois;
 
 public class ControlAcheterProduit {
-	private Village village;
-	private ControlTrouverEtalVendeur controlTrouverEtalVendeur;
-	private ControlVerifierIdentite controlVerifierIdentite;
+    private Village village;
+    private ControlTrouverEtalVendeur controlTrouverEtalVendeur;
+    private ControlVerifierIdentite controlVerifierIdentite;
 
-	public ControlAcheterProduit(ControlVerifierIdentite controlVerifierIdentite,
-			ControlTrouverEtalVendeur controlTrouverEtalVendeur,
-			Village village) {
-		this.village = village;
-		this.controlVerifierIdentite = controlVerifierIdentite;
-		this.controlTrouverEtalVendeur = controlTrouverEtalVendeur;
-	}
+    public ControlAcheterProduit(ControlVerifierIdentite controlVerifierIdentite,
+                                 ControlTrouverEtalVendeur controlTrouverEtalVendeur,
+                                 Village village) {
+        this.village = village;
+        this.controlVerifierIdentite = controlVerifierIdentite;
+        this.controlTrouverEtalVendeur = controlTrouverEtalVendeur;
+    }
 
-	//TODO a completer
+    public boolean verifierIdentiteClient(String nomClient) {
+        return controlVerifierIdentite.verifierIdentite(nomClient);
+    }
+
+    public String[] listerVendeurs(String produit) {
+        Gaulois[] vendeurs = village.rechercherVendeursProduit(produit);
+        if (vendeurs == null || vendeurs.length == 0) {
+            return null;
+        }
+        String[] nomsVendeurs = new String[vendeurs.length];
+        for (int i = 0; i < vendeurs.length; i++) {
+            nomsVendeurs[i] = vendeurs[i].getNom();
+        }
+        return nomsVendeurs;
+    }
+
+    public String acheterProduit(String nomClient, String nomVendeur, String produit, int quantite) {
+        if (!verifierIdentiteClient(nomClient)) {
+            return "Je suis désolée " + nomClient + " mais il faut être un habitant de notre village pour commercer ici.";
+        }
+
+        Etal etal = controlTrouverEtalVendeur.trouverEtalVendeur(nomVendeur);
+        if (etal == null || !etal.getProduit().equals(produit)) {
+            return "Désolé, aucun vendeur ne correspond à votre recherche.";
+        }
+
+        int quantiteAchetee = etal.acheterProduit(quantite);
+        if (quantiteAchetee < quantite) {
+            return nomClient + " veut acheter " + quantite + " " + produit + 
+                   " mais malheureusement, " + nomVendeur + " n'en a que " + quantiteAchetee + ".";
+        } else {
+            return nomClient + " achète " + quantiteAchetee + " " + produit + " à " + nomVendeur + ".";
+        }
+    }
 }
--- src/frontiere/BoundaryAcheterProduit.java
+++ src/frontiere/BoundaryAcheterProduit.java
@@ -1,18 +1,49 @@
 package frontiere;
 
 import java.util.Scanner;
-
 import controleur.ControlAcheterProduit;
 
 public class BoundaryAcheterProduit {
-	private Scanner scan = new Scanner(System.in);
-	private ControlAcheterProduit controlAcheterProduit;
+    private Scanner scan = new Scanner(System.in);
+    private ControlAcheterProduit controlAcheterProduit;
 
-	public BoundaryAcheterProduit(ControlAcheterProduit controlAcheterProduit) {
-		this.controlAcheterProduit = controlAcheterProduit;
-	}
+    public BoundaryAcheterProduit(ControlAcheterProduit controlAcheterProduit) {
+        this.controlAcheterProduit = controlAcheterProduit;
+    }
 
-	public void acheterProduit(String nomAcheteur) {
-		// TODO à completer
-	}
+    public void acheterProduit(String nomAcheteur) {
+        if (!controlAcheterProduit.verifierIdentiteClient(nomAcheteur)) {
+            System.out.println("Je suis désolée " + nomAcheteur + " mais il faut être un habitant de notre village pour commercer ici.");
+            return;
+        }
+        
+        System.out.println("Quel produit voulez-vous acheter ?");
+        String produit = scan.nextLine();
+
+        String[] vendeurs = controlAcheterProduit.listerVendeurs(produit);
+        if (vendeurs == null || vendeurs.length == 0) {
+            System.out.println("Désolé, personne ne vend ce produit au marché.");
+            return;
+        }
+
+        System.out.println("Chez quel commerçant voulez-vous acheter des " + produit + " ?");
+        for (int i = 0; i < vendeurs.length; i++) {
+            System.out.println((i + 1) + " - " + vendeurs[i]);
+        }
+        int choixVendeur = scan.nextInt() - 1; 
+        scan.nextLine(); 
+
+        if (choixVendeur < 0 || choixVendeur >= vendeurs.length) {
+            System.out.println("Choix invalide.");
+            return;
+        }
+
+        String nomVendeur = vendeurs[choixVendeur];
+        System.out.println("Combien de " + produit + " voulez-vous acheter ?");
+        int quantite = scan.nextInt();
+        scan.nextLine(); 
+
+        String message = controlAcheterProduit.acheterProduit(nomAcheteur, nomVendeur, produit, quantite);
+        System.out.println(message);
+    }
 }
